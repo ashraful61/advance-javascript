44a5f9e885ed9df009d5ad36ae825429b56cfaba
ashraful61 <ashraful.ru37@gmail.com>
2020-03-06 07:42:41 +0600

    last commit

--- arraySplit.js
+++ arraySplit.js
@@ -1,5 +1,8 @@
 const nums = [1,2,3,4,5,6,7,8,9];
 const part = nums.slice(2,3);
-console.log(part);
+//console.log(part);
 const parts = nums.splice(1,4);
-console.log(parts)
+//console.log(parts)
+//console.log(nums);
+const together = parts.join(" ami ") 
+console.log(together)
